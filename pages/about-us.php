31f82124f84288ba290e58e0e662a1ba7355cb17
achinsood <achin@gharsearch.com>
2015-05-20 02:17:09 +0530

    Create about-us.php

---
+++ pages/about-us.php
@@ -0,0 +1,11 @@
+<?php $page_title = "About Us"; ?>
+<div class="content_inner">
+<div class="content_container white_container">
+<h1>About Us</h1>
+<div class="page_container">
+<div class="page_row">
+<p>your About Us page will come here</p>
+</div>
+</div>
+</div>
+</div>
