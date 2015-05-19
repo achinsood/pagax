1e02f9da22ac326a9789dc49d134ca01f9b4c463
achinsood <achin@gharsearch.com>
2015-05-20 02:19:53 +0530

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,141 @@
+</div>
+<div class="footer">
+<div class="content_inner">
+
+<div class="footer_column">
+<span class="heading">Company</span>
+
+<div class="middle_row">
+<a class="links" href="#" title="About Us">About Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Contact Us">Contact Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Site Map" >Site Map</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Feedback" >Feedback</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Terms & Conditions">Terms & Conditions</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Privacy Policy">Privacy Policy</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="FAQ's">FAQ's</a>
+</div>
+
+</div>
+<div class="footer_column">
+<span class="heading">Company</span>
+
+<div class="middle_row">
+<a class="links" href="#" title="About Us">About Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Contact Us">Contact Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Site Map" >Site Map</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Feedback" >Feedback</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Terms & Conditions">Terms & Conditions</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Privacy Policy">Privacy Policy</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="FAQ's">FAQ's</a>
+</div>
+
+</div>
+<div class="footer_column">
+<span class="heading">Company</span>
+
+<div class="middle_row">
+<a class="links" href="#" title="About Us">About Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Contact Us">Contact Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Site Map" >Site Map</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Feedback" >Feedback</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Terms & Conditions">Terms & Conditions</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Privacy Policy">Privacy Policy</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="FAQ's">FAQ's</a>
+</div>
+
+</div>
+<div class="footer_column">
+<span class="heading">Company</span>
+
+<div class="middle_row">
+<a class="links" href="#" title="About Us">About Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Contact Us">Contact Us</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Site Map" >Site Map</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Feedback" >Feedback</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Terms & Conditions">Terms & Conditions</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="Privacy Policy">Privacy Policy</a>
+</div>
+
+<div class="middle_row">
+<a class="links" href="#" title="FAQ's">FAQ's</a>
+</div>
+
+</div>
+</div>
+
+</div>
+<div class="copyrights">All rights reserved - Copyright &copy; 2015 - <a class="links" title="Achin Sood" href="https://www.facebook.com/achin.sood">Achin Sood</a></div>
+</div>
+</div>
+</body>
+</html>
+<?php echo sanitize_output(ob_get_clean()); ?>
